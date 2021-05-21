01014545915214f101b6fd12474afa73f5701f87
Utkarsh Tiwari <utkarsh.tiwari@somaiya.edu>
2021-05-21 19:25:54 +0530

    increased padding of footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,4 +1,4 @@
-
+// Footer
 const navigation = [
   {
     name: 'Twitter',
@@ -28,7 +28,7 @@ const FooterComponent = () => {
   return (
     <>
       <footer className="bg-blueGray-800">
-        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
           <div className="flex justify-center space-x-6 md:order-2">
             {navigation.map((item) => (
               <a key={item.name} href={item.href} target="_blank" rel="noreferrer" className="text-gray-400 hover:text-gray-300">
