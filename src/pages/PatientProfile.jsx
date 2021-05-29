4b9b4c445352491135bb6c1a92d7beccbf6f29a3
Preet Shah <shahpreetk@gmail.com>
2021-05-29 18:31:02 +0530

    user login persists on patient profile page

--- src/pages/PatientProfile.jsx
+++ src/pages/PatientProfile.jsx
@@ -1,9 +1,19 @@
 // @ts-check
+import { useContext, useEffect } from "react";
 import MoreInfoComponent from "../components/MoreInfoComponent";
 import InfoTable from "../components/InfoTable";
 import AddInfoTable from "../components/AddInfoTable";
+import AuthContext from "../context/auth/authContext";
 
 const PatientProfile = () => {
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    document.title = "Patient Management System";
+    document.head.lang = "en";
+    authContext.loadUser();
+    // eslint-disable-next-line
+  }, []);
   return (
     <div className="bg-gray-100">
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
